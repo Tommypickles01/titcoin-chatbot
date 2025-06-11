6c183f4959659c92b929fc75d411a694f0531b81
Tommypickles01 <vuongthomas1@yahoo.com>
2025-06-11 05:27:56 -0500

    Delete Downloads/titcoin_chatbot_vercel_ready directory

--- Downloads/titcoin_chatbot_vercel_ready/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import TitcoinChatbot from "./TitcoinChatbot";
-import "@google/model-viewer";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<TitcoinChatbot />);
